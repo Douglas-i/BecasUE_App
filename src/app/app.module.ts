57d4e0f3641dc995c77440ea2b0a4d0718de8e51
Douglas-i <hdouglasu@gmai.com>
2023-11-22 09:23:26 -0600

    Regreseando a como estaba antes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login/login.component';
-import { RegistroComponent } from './registro/registro.component';
-import { PantallaComponent } from './pantalla/pantalla.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    LoginComponent,
-    RegistroComponent,
-    PantallaComponent,    
+    HomeComponent   
   ],
   imports: [
     BrowserModule,
